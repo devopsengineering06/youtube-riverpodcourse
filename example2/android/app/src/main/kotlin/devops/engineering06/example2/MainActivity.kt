2748fc69b0eddbed973ffb27435fdcbb24109141
Dev Ops <devops.engineering06@gmail.com>
2023-01-24 22:26:55 +0300

    example2

---
+++ example2/android/app/src/main/kotlin/devops/engineering06/example2/MainActivity.kt
@@ -0,0 +1,6 @@
+package devops.engineering06.example2
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example2/lib/main.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main(List<String> args) {
+  runApp(
+    const ProviderScope(
+      child: App(),
+    ),
+  );
+}
+
+extension OptionalInfixAddition<T extends num> on T? {
+  T? operator +(T? other) {
+    final shadow = this ?? other;
+    if (shadow != null) {
+      return shadow + (other ?? 0) as T;
+    } else {
+      return null;
+    }
+  }
+}
+
+// void testIt() {
+//   final int? int1 = null;
+//   final int? int2 = null;
+//   final result = int1 + int2;
+//   print(result);
+// }
+
+class Counter extends StateNotifier<int?> {
+  Counter() : super(null);
+
+  void increment() => state = state == null ? 1 : state + 1;
+}
+
+final counterProvider = StateNotifierProvider<Counter, int?>(
+  (ref) => Counter(),
+);
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      darkTheme: ThemeData.dark(),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends ConsumerWidget {
+  const HomePage({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Consumer(
+          builder: (context, ref, child) {
+            final count = ref.watch(counterProvider);
+            final text = count == null ? 'Press the button' : count.toString();
+            return Center(child: Text(text));
+          },
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextButton(
+            // onPressed: () {
+            //   ref.read(counterProvider.notifier).increment();
+            // },
+            onPressed: ref.read(counterProvider.notifier).increment,
+            child: const Text('Increment count'),
+          ),
+        ],
+      ),
+    );
+  }
+}
